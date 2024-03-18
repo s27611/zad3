11472f0ce5972cfce621471ecbddb810aaadbdf2
s27611 <162113453+s27611@users.noreply.github.com>
2024-03-18 11:39:48 +0100

    class kontener cd

--- Zad3/Zad3/Program.cs
+++ Zad3/Zad3/Program.cs
@@ -3,10 +3,11 @@ public class Program
 {
     public static void Main(String[] args)
     {
-        
-        
-        
-        
+        int GeneratorNumeru = 0;
+
+
+
+
     }
 }
 
@@ -15,25 +16,54 @@ public class Program
 
 
 
-public abstract class Kontener
-{
+public abstract class Kontener{
+
     protected double Waga;
     protected double Wysokosc;
     protected double Glebokosc;
-    protected Kontener(double waga, double wysokosc, double glebokosc)
+    protected double MaxLadownosc;
+    protected String KodSeryjny;
+    
+    protected Kontener(double waga, double wysokosc, double glebokosc, double maxLadownosc)
     {
         Waga = waga;
         Wysokosc = wysokosc;
         Glebokosc = glebokosc;
+        MaxLadownosc = maxLadownosc;
+        KodSeryjny = "KON-";
+    }
+
+    protected void Oproznij(Kontener kontener)
+    {
+        
+    }
+
+    protected void Zaladuj(Kontener kontener)
+    {
+        
     }
     
+    
+    
+    
+    
+    
+    
+    
 
-
-
-
-
-
-
-
+    protected Kontener(double waga, double wysokosc, double glebokosc)
+    {
+        throw new NotImplementedException();
+    }
 }
+
+public class KontenerNaPlyn : Kontener
+
+{
+    public KontenerNaPlyn(double waga, double wysokosc, double glebokosc, double maxLadownosc) : base(waga, wysokosc, glebokosc, maxLadownosc)
+    {
+    }
+}
+
+
 
